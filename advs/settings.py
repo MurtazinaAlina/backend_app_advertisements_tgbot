d05e19821ee336416c52798f312af48cbb59aacb
AlinaProkofeva <connect-trener@mail.ru>
2024-02-19 13:37:31 +0300

    add cicd black flake8

--- advs/settings.py
+++ advs/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+mt042hco^!!4k7-n!ed25*cd7qudm(#z0t1*6le%qf2q3q&0e"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
